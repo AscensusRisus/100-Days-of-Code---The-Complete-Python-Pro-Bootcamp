ba777986c74d0294c798a8848583a7e210acf767
Doğuş Köksal <110297054+AscensusRisus@users.noreply.github.com>
2024-08-19 22:46:03 +0300

    Day 5

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,15 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-print(range(1, 10))
+
+# total_exam_scores = sum(student_scores)
+# print(total_exam_scores)
+
+# total_scores = 0
+
+# for score in student_scores:
+#     total_scores += score
+# print(total_scores)
+largest_number = student_scores[0]
+for score in student_scores:
+    if score > largest_number:
+        largest_number = score
+print(largest_number)
--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,4 +1,9 @@
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+import random as rd
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+           'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+           'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -7,4 +12,48 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# Easy Version
+# password = ""
+#
+# for item in range(0, nr_letters):
+#     random_number = rd.randint(0, len(letters) - 1)
+#     password += letters[random_number]
+# for item in range(0, nr_symbols):
+#     random_number = rd.randint(0, len(symbols) - 1)
+#     password += symbols[random_number]
+# for item in range(0, nr_numbers):
+#     random_number = rd.randint(0, len(numbers) - 1)
+#     password += numbers[random_number]
+# print(password)
+
+# Hard Version
+password = ""
+password_randomizer = []
+for item in range(0, nr_letters):
+    random_number = rd.randint(0, len(letters) - 1)
+    password_randomizer.append(letters[random_number])
+for item in range(0, nr_symbols):
+    random_number = rd.randint(0, len(symbols) - 1)
+    password_randomizer.append(symbols[random_number])
+for item in range(0, nr_numbers):
+    random_number = rd.randint(0, len(numbers) - 1)
+    password_randomizer.append(numbers[random_number])
+rd.shuffle(password_randomizer)
+for item in password_randomizer:
+    password += item
+print(f"Your password is: {password}")
+
+# Alternative way to do it
+# password_list = []
+# password = ""
+# for char in range(0, nr_letters):
+#     password_list.append(rd.choice(letters))
+# for char in range(0, nr_symbols):
+#     password_list.append(rd.choice(symbols))
+# for char in range(0, nr_numbers):
+#     password_list.append(rd.choice(numbers))
+# rd.shuffle(password_list)
+# for char in password_list:
+#     password += char
+# print(f"Your password is {password}")
 
